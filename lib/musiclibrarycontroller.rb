eae9ac96cc93f918973738aa511563f0eb036dbf
Eyal Toledano <eyaltoledano@me.com>
2019-03-28 18:28:16 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -25,9 +25,7 @@ class MusicLibraryController
     list_songs if input == "list songs"
     list_artists if input == "list artists"
     list_genres if input == "list genres"
-    if input == "list " + artist_or_genre
-      binding.pry
-    end
+    list_songs_by_artist if input == "list artist"
     while input != "exit"
       binding.pry
       input = gets.chomp
